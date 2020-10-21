ac6f6bfc06343e1614b1b78d77405591e003693b
Andreas Hollandt <trass3r@gmail.com>
2020-10-21 16:57:18 +0200

    fix hardcoded kernel versions in build script build on bionic for more libc compatibility

--- buildkernel.sh
+++ buildkernel.sh
@@ -1,12 +1,23 @@
 #! /bin/bash
 set -e
 
+TARGET=$(uname -r)
+
 if [ ! -d WSL2-Linux-Kernel ] ; then
 git clone --depth=100 -b linux-msft-wsl-4.19.y https://github.com/microsoft/WSL2-Linux-Kernel.git
 fi
-(cd WSL2-Linux-Kernel && git co linux-msft-4.19.104)
+(cd WSL2-Linux-Kernel && git fetch && git co $TARGET)
 
 make -C WSL2-Linux-Kernel/tools/perf -j8
-make -C WSL2-Linux-Kernel/tools/perf install DESTDIR=`pwd`/linux-tools-4.19.104-microsoft-standard prefix=/usr
-dpkg-deb --build linux-tools-4.19.104-microsoft-standard
-echo use with sudo apt install ./linux-tools-4.19.104-microsoft-standard.deb
+make -C WSL2-Linux-Kernel/tools/perf install DESTDIR=`pwd`/linux-tools-$TARGET prefix=/usr
+mkdir -p $(pwd)/linux-tools-$TARGET/DEBIAN
+echo "Package: linux-tools
+Version: $TARGET
+Priority: optional
+Architecture: amd64
+Depends: libunwind-dev (>= 1.2.1), libbabeltrace1 (>= 1.5.5), libbabeltrace-ctf1 (>= 1.5.5), libnuma1 (>= 2.0.11)
+Maintainer: No one
+Description: linux-perf tools for WSL2
+" > $(pwd)/linux-tools-$TARGET/DEBIAN/control
+dpkg-deb --build linux-tools-$TARGET
+echo use with sudo apt install ./linux-tools-$TARGET.deb
